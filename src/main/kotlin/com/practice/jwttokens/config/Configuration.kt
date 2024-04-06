731b7f3f12f0bbebf52f72099717bc52ade20b86
DJDrama <putzzang@naver.com>
2024-04-06 12:35:13 +0900

    Update Config

--- src/main/kotlin/com/practice/jwttokens/config/Configuration.kt
+++ src/main/kotlin/com/practice/jwttokens/config/Configuration.kt
@@ -1,11 +1,42 @@
 package com.practice.jwttokens.config
 
+import com.practice.jwttokens.repository.UserRepository
+import com.practice.jwttokens.service.CustomUserDetailService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
 class Configuration {
 
 
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService {
+        return CustomUserDetailService(userRepository = userRepository)
+    }
+
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider {
+        return DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(/* userDetailsService = */ userDetailsService(userRepository))
+                it.setPasswordEncoder(encoder())
+            }
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
+    }
 }
